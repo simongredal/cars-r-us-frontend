e8270f27e92a094c4df14e881f308092ae3b902e
plaul <lmor@kea.dk>
2022-03-07 16:49:26 +0100

    redy for project-1

---
+++ js-for-pages/page2.js
@@ -0,0 +1,7 @@
+
+export function setupP2Handlers() {
+  document.getElementById("t1-btn1").onclick = () => {
+    document.getElementById("t1-content").innerText = "Wan't to have a job - Learn JavaScript"
+  }
+}
+
